c2d6515ce0508966280ac419e94402c1299de9d6
yihong <wang-yh@oriental-finance.com>
2019-05-30 14:45:53 +0800

    update dockerfile

--- web_project/settings.py
+++ web_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#hh+9_=h87!fqjx&)hr+#-mco6!4l7lo%9$8(&j6r)ve(e=)5e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
